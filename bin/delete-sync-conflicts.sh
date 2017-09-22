dab0657f40fa85b1272384688b94ca0d26211d2f
Alex Kavanagh <alex@ajkavanagh.co.uk>
2017-09-22 17:55:07 +0100

    Add delete-sync-conflicts.sh helper for SyncThing conflicts

---
+++ bin/delete-sync-conflicts.sh
@@ -0,0 +1,13 @@
+#/usr/bin/env bash
+# clean up sync conflicts brought about by Syncthing
+
+find . -name '*sync-conflict*'
+
+read -r -p "Are you sure you want to delete the sync-conflicts? [y/N] " response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]; then
+	find . -name '*sync-conflict*' -prune -exec rm -rf "{}" \;
+	echo "Done!"
+else
+	echo "Doing nothing."
+fi
