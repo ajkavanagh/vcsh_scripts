3ac5add354609568ba5c89d942129dfa07bb6b63
Alex Kavanagh <alex@ajkavanagh.co.uk>
2017-11-10 18:20:38 +0000

    add .stestr to clean-up.sh

--- bin/clean-up.sh
+++ bin/clean-up.sh
@@ -5,6 +5,7 @@ find . -name '.tox' -prune -exec rm -rf "{}" \;
 find . -iname '.venv*' -prune -exec rm -rf "{}" \;
 find . -name '.testrepository' -prune -exec rm -rf "{}" \;
 find . -name '__pycache__' -prune -exec rm -rf "{}" \;
+find . -name '.stestr' -prune -exec rm -rf "{}" \;
 find . -iname '*.pyc' -delete
 find . -name 'func-results.json' -delete
 
