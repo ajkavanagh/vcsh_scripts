4b37da38d9552994d5483a7ec5f0a4336a1feefc
Alex Kavanagh <alex@ajkavanagh.co.uk>
2017-11-14 11:26:21 +0000

    Add open-pytest.py -- handy opener for openstack test modules printouts

---
+++ bin/open-pytest.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Converts a Python path to a folder path and open Vim on the relevant
+# lines.
+#
+# Usage:
+#
+#   pyvi <python_module_path>
+#
+# For example:
+#
+#   pyvi nova.tests.unit.virt.libvirt.test_vif.LibvirtVifTestCase
+#
+# This will convert the 'nova.tests.unit.virt.libvirt.test_vif' part of this
+# module path to a filepath then open the file (test_vif.py) on the
+# appropriate line.
+
+# originally from: https://that.guru/blog/open-python-paths-with-vim/
+# but with modifications
+
+import os.path
+import subprocess
+import sys
+
+
+def runner(args):
+    if len(args) != 2:
+        print("Must pass the test path to the file")
+        sys.exit(1)
+
+    editor = os.environ.get('EDITOR', None)
+    if editor is None:
+        print("Must set the EDITOR variable for the script to run")
+        sys.exit(1)
+
+    path = sys.argv[1].split('.')
+
+    for i in range(len(path), 1, -1):
+        filename = os.path.join(*path[0:i-1] + ['{}.py'.format(path[i-1])])
+        if os.path.isfile(filename):
+            args = ['{}'.format(filename)]
+            args = args + ['+1'] + ['+/{}'.format(p) for p in path[i:]]
+            os.execv(editor, [editor] + args)
+
+    print("Couldn't find the path as a file?")
+    exist(1)
+
+
+if __name__ == '__main__':
+    runner(sys.argv)
