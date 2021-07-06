43d7d8b166c9f72c39fa866ea1fa16a9fe9273a9
Alex Kavanagh <alex@ajkavanagh.co.uk>
2021-07-06 20:38:18 +0100

    Add ansible-vault-pass.sh script for secrets in bitwardent

--- bin/ansible-vault-pass.sh
+++ bin/ansible-vault-pass.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 BW_VAULT_ENTRY_ID="ansible-vault"
-bw_session="$(bw unlock --raw)"
+if [[ -z "$BW_SESSION" ]]; then
+    bw_session="$(bw unlock --raw)"
+else
+    bw_session="$BW_SESSION"
+fi
 echo "$(bw get password ${BW_VAULT_ENTRY_ID} --session ${bw_session} --raw)"
